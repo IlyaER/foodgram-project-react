b4367851f0959af361b693a8a94f4ccdd7a445a6
Ilya Rogovoy <16943792+IlyaER@users.noreply.github.com>
2022-09-15 15:10:48 +0300

    created some models

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -38,18 +38,27 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recipes.apps.RecipesConfig',
+    'rest_framework',
+    'corsheaders',
     'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_URLS_REGEX = r'^/api/.*$'
+#CORS_ALLOWED_ORIGINS = [
+#    'http://localhost:3000',
+#]
 
 ROOT_URLCONF = 'foodgram.urls'
 
@@ -120,3 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = 'recipes.User'
+
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,60 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    USER = 'user'
+    ADMIN = 'admin'
+    USERROLES = [
+        (USER, 'User'),
+        (ADMIN, 'Admin')
+    ]
+    access = models.CharField(
+        'Доступы',
+        max_length=20,
+        choices=USERROLES,
+        default=USER
+    )
+
+    # @property
+    # def is_admin(self):
+    #     return self.access == self.ADMIN
+
+    class Meta:
+        ordering = ('pk',)
+
+
+class Components(models.Model):
+    """
+    Список всех возможных составных частей для блюд
+    """
+    name = models.CharField(max_length=64)
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=64)
+    picture = models.ImageField(
+        upload_to='recipes/images/',
+        null=True,
+        default=None
+        )
+    description = models.TextField(max_length=1000, blank=True, null=True)
+    ingredient = models.ManyToManyField(
+        Components,
+        through='Ingredients',
+        related_name='recipe',
+    )
+
+
+class Ingredients(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    name = models.ForeignKey(Components, on_delete=models.SET_NULL, null=True)
+    quantity = models.CharField(max_length=8)
+    measure = models.CharField(max_length=32)
+
+
+class Tag(models.Model):
+    pass
