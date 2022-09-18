41560e0beffe4ecfb613a05458281da8c46dc188
Ilya Rogovoy <16943792+IlyaER@users.noreply.github.com>
2022-09-18 16:04:38 +0300

    auth implemented

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework.authtoken import views
+
 from .views import *
 
 
@@ -12,4 +14,5 @@ router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/token/login/', views.obtain_auth_token),
 ]
