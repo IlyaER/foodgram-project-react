f4598248a0390519a5096d30ced30344d22cbc2c
Ilya Rogovoy <16943792+IlyaER@users.noreply.github.com>
2022-10-04 16:49:26 +0300

    another change settings static folder

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -145,7 +145,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_back/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, './frontend/build/static')]
 
